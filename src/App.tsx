2b7a21f0fee6d0dc8463619e2e7925fa5de32981
HurasAdam <huradam@wp.pl>
2024-05-28 19:47:48 +0200

    initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './index.css'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+ <div className='w-full flex flex-col'>
+<Header/>
+ </div>
+  )
+}
+
+export default App
