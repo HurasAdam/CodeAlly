114ff54bca3b8a2ebf070c983dfc87bfa46949ff
HurasAdam <huradam@wp.pl>
2024-05-30 21:34:20 +0200

    Chore: Removed unused imports.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import video1 from "../assets/video1.mp4";
-import video2 from "../assets/video2.mp4";
 import video3 from "../assets/video3.mp4";
 
 const HeroSection:React.FC = () => {
